f15ae368f10e6362420a664e86266da1403d9fbd
Yong-Dd <71385485+Yong-Dd@users.noreply.github.com>
2021-11-02 23:30:11 +0900

    week1 Compose basics 완료

--- week 1-Jetpack Compose basics/BasicsCodelab22/app/src/main/java/com/yongdd/basicscodelab2/MainActivity.kt
+++ week 1-Jetpack Compose basics/BasicsCodelab22/app/src/main/java/com/yongdd/basicscodelab2/MainActivity.kt
@@ -1,40 +1,54 @@
 package com.yongdd.basicscodelab2
 
 import android.content.res.Configuration
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme.shapes
+import androidx.compose.material.MaterialTheme.typography
+import androidx.compose.material.icons.Icons
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.yongdd.basicscodelab2.ui.theme.BasicsCodelab2Theme
+import androidx.compose.material.icons.Icons.Filled
+import androidx.compose.material.icons.filled.*
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BasicsCodelab2Theme {
 //                // A surface container using the 'background' color from the theme
-//                Surface(color = MaterialTheme.colors.background) {
-//                    Greeting(Message("Hey","Android"))
-//                }
-                Conversation(SampleData.conversationSample)
+                    MyApp()
+//                    var names = listOf<String>("android","pp","didi","nunu","nana")
+//                    Greeting(names)
+
+//                Conversation(SampleData.conversationSample)
             }
 
 
@@ -42,6 +56,129 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+
+
+@Composable
+fun OnboardingScreen(onContinueClicked : ()->Unit){
+
+    Surface{
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Welcome to the Basic Codelab!")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick = onContinueClicked
+            ){
+                Text("Continue")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview(){
+    BasicsCodelab2Theme {
+        OnboardingScreen(onContinueClicked = {})
+    }
+}
+
+//var names = listOf<String>("android","pp","didi","nunu","nana")
+@Composable
+private fun MyApp(){
+    var shouldShowOnboarding by rememberSaveable{ mutableStateOf(true)}
+    if(shouldShowOnboarding){
+        OnboardingScreen(onContinueClicked  = {shouldShowOnboarding = false})
+    }else{
+        Greetings()
+    }
+
+}
+
+@Composable
+fun Greetings(names: List<String> = List(1000){"$it"}){
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)){
+       items(items = names){name ->
+           Greeting(name = name)
+       }
+    }
+}
+
+@Composable
+fun Greeting(name: String){
+    Card(
+        backgroundColor = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp,horizontal = 8.dp)
+    ){
+        CardContent(name)
+    }
+
+
+}
+
+@Composable
+private fun CardContent(name:String){
+    var expanded by remember{ mutableStateOf(false)}
+//    val extraPadding by animateDpAsState(if(expanded)48.dp else 0.dp,
+//        animationSpec = spring(dampingRatio = Spring.DampingRatioMediumBouncy,
+//            stiffness = Spring.StiffnessLow)
+//    )
+    Surface(color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp,horizontal = 8.dp)){
+        Row(modifier = Modifier.padding(12.dp)
+            .animateContentSize(animationSpec = spring(
+                dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow)
+            )){
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(12.dp)) {
+                Text("Hello!")
+                Text(name,style = typography.h4.copy(fontWeight = FontWeight.ExtraBold))
+                if(expanded){
+                    Text(
+                        text = ("Composem ipsum color sit lazy, " +
+                                "padding theme elit, sed do bouncy. ").repeat(4)
+                    )
+                }
+            }
+            IconButton(onClick = {expanded = !expanded}){
+                Icon(
+                    imageVector = if(expanded) Filled.KeyboardArrowDown else Filled.KeyboardArrowUp,
+                    contentDescription = if(expanded){
+                        stringResource(R.string.show_less)
+                    }else{
+                        stringResource(id = R.string.show_more)
+                    }
+                )
+            }
+
+        }
+
+    }
+}
+
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun DefaultPreview() {
+    BasicsCodelab2Theme {
+        MyApp()
+
+    }
+}
+
+
+
 
 data class Message(val author:String, val body :String)
 
@@ -71,30 +208,26 @@ fun MessageCard(name: Message) {
             Text(
                 text=name.author,
                 color = MaterialTheme.colors.secondaryVariant,
-                style = MaterialTheme.typography.subtitle2
+                style = typography.subtitle2
             )
             Spacer(modifier = Modifier.height(4.dp))
             Surface(
-                shape = MaterialTheme.shapes.medium,
+                shape = shapes.medium,
             elevation = 1.dp,
                 color = surfaceColor,
-                modifier = Modifier.animateContentSize().padding(1.dp)
+                modifier = Modifier
+                    .animateContentSize()
+                    .padding(1.dp)
             ){
                 Text(text=name.body,
-                    style = MaterialTheme.typography.body2)
+                    style = typography.body2)
             }
         }
     }
 }
 
 
-//@Composable
-//fun DefaultPreview() {
-//    BasicsCodelab2Theme {
-//        Greeting(Message("Android","Jetpack Designer"))
-//
-//    }
-//}
+
 
 
 @Composable
@@ -107,18 +240,18 @@ fun Conversation(messages:List<Message>){
     }
 }
 
-@Preview(name = "Light Mode")
-@Preview(
-    uiMode = Configuration.UI_MODE_NIGHT_YES,
-    showBackground = true,
-    name = "Dark Mode"
-)
-@Composable
-fun PreviewConversation(){
-    BasicsCodelab2Theme {
-        Conversation(SampleData.conversationSample)
-    }
-}
+//@Preview(name = "Light Mode")
+//@Preview(
+//    uiMode = Configuration.UI_MODE_NIGHT_YES,
+//    showBackground = true,
+//    name = "Dark Mode"
+//)
+//@Composable
+//fun PreviewConversation(){
+//    BasicsCodelab2Theme {
+//        Conversation(SampleData.conversationSample)
+//    }
+//}
 
 
 
--- week 1-Jetpack Compose basics/BasicsCodelab22/app/src/main/java/com/yongdd/basicscodelab2/ui/theme/Color.kt
+++ week 1-Jetpack Compose basics/BasicsCodelab22/app/src/main/java/com/yongdd/basicscodelab2/ui/theme/Color.kt
@@ -6,3 +6,8 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val DarkGreen = Color(0xFF03341C)
+val Green = Color(0xFF387C43)
+val LightGreen = Color(0xFF7FA87A)
+val Chartreuse = Color(0xFFEFF7CF)
